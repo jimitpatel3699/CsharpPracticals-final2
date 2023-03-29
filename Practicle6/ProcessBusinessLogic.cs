7998c98cacf5fd9a03b2727a335e601511923bd1
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-03-29 11:13:08 +0530

    events and delegate in C#

---
+++ Practicle6/ProcessBusinessLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicle6
+{
+    public delegate void Notify(); //delegate
+    internal class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted; // event
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            OnProcessCompleted();
+        }
+        //protected virtual method
+        protected virtual void OnProcessCompleted()
+        {
+            //if ProcessCompleted is not null then call delegate
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Practicle6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practicle6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+        // event handler
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Method Invoked");
+        }
+    }
+}
