b0e790f878d40cbb39935b62f6a1d306ae028bc8
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-03-29 11:01:03 +0530

    simple calculator and debug app

---
+++ Practicle1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Practicle1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calsi = new Calculator();
+            DebuggingApplication dbug = new DebuggingApplication();
+            int choice;
+            while (true)
+            {
+                Console.WriteLine("============================");
+                Console.WriteLine("\nEnter 1 for Calculator app");
+                Console.WriteLine("Enter 2 for Debugging app");
+                Console.WriteLine("Enter 0 for close Window");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("============================");
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            
+                            calsi.Calculation();
+                            break;
+                        }
+                    case 2:
+                        {
+                            dbug.Method_debug();
+                            break;
+                        }
+                    case 0:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                }
+                
+            }
+            
+        }
+    }
+}
