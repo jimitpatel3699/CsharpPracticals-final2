d90930b989d474029e5b135c7a1580f4555192d9
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-03-29 11:04:42 +0530

    class and method demo

---
+++ practicle2/Customer_Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace practicle2
+{
+    internal class CustomerAccount
+    {
+        readonly string bank_name;
+        string customer_name;
+        long customer_accountNo;
+        public CustomerAccount(string cust_name,long cust_accNo)
+        {
+            bank_name = "DCB BANK";
+            customer_accountNo = cust_accNo;
+            customer_name = cust_name;
+        }
+
+        //printInfo() method declare for display the passbook of user.
+        //method not take any parameter also not have returntype.
+            public void PrintInfo()
+        {
+            Console.WriteLine("=======================");
+            Console.WriteLine($"WELCOME TO {bank_name}");
+            Console.WriteLine("=======================");
+            Console.WriteLine("=======Pass-Book=========\n");
+                
+            Console.WriteLine($"Customer Name: {customer_name}\n");
+            Console.WriteLine($"AccountNo: {customer_accountNo}\n");
+            
+            Console.WriteLine("=========================");
+        }
+    }
+}
---
+++ practicle2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace practicle2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "jimit patel";
+            long accountNO = 10000000234588;
+            CustomerAccount customer1 = new CustomerAccount(name,accountNO);
+            customer1.PrintInfo();
+        }
+    }
+}
