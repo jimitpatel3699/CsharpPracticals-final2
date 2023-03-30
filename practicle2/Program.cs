c09486525502d9c6f0f0b4e7ed2db9df84125824
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-03-30 12:44:01 +0530

    constructor and acces specifier added

--- practicle2/Program.cs
+++ practicle2/Program.cs
@@ -8,6 +8,55 @@
             long accountNO = 10000000234588;
             CustomerAccount customer1 = new CustomerAccount(name,accountNO);
             customer1.PrintInfo();
+
+            //implicit constructor 
+
+            ImplicitConstuctor implicitConstuctor = new ImplicitConstuctor();
+            implicitConstuctor.PrintData();
+            
+            
+            //parameterless constructor
+
+            ParameterlessConstructor parameterlessConstructor = new ParameterlessConstructor();
+          
+            
+            //try to access private member 
+
+            //Console.WriteLine($"Student id ={parameterlessConstructor.Id});
+
+            //access public member of class 
+
+            Console.WriteLine(parameterlessConstructor.Name);
+         
+            
+            //paramerised constructor 
+
+            ParametrisedConstrctor parametrisedConstructor = new ParametrisedConstrctor("21it100085", "jimit", "23-11-2001", 9582478996);
+            parametrisedConstructor.PrintData();
+            
+            //try to access protected member
+
+            //Console.WriteLine($"Student Mobile:{parametrisedConstructor.Mobile}");
+
+            //copyconstructor
+
+            CopyConstuctor copyConstuctor = new CopyConstuctor(10, 20);
+            CopyConstuctor copyConstuctor1 = new CopyConstuctor(copyConstuctor);
+
+            //static constructor
+
+            StaticConstructor staticConstructor = new StaticConstructor(2110021);
+
+            //private constructor
+
+            //PrivateConstructor privateConstructor = new PrivateConstructor();
+
+            //overloaded constructor
+
+            OverloadConstructor overloadConstructor1 = new OverloadConstructor(14, 12, 19);
+            OverloadConstructor overloadConstructor2 = new OverloadConstructor(50,69);
+            OverloadConstructor overloadConstructor3 = new OverloadConstructor("jimit","Patel");
+
         }
     }
 }
---
+++ practicle2/StaticConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicle2
+{
+    internal class StaticConstructor
+    {
+        static public string Division;
+        public int Id;
+        static StaticConstructor() //static staticConstructor(int i) not allowed because staic constructor not take any arguments
+        {
+            Division = "Csp";
+            //j = 1; not [ossible only allowed static member intialization
+            Console.WriteLine($"value of  division is in static constuctor {Division}\n");
+        }
+        public StaticConstructor(int Sid)
+        {
+         
+            
+            Id = Sid;
+            
+            Console.WriteLine($"value of  ID is in public constructor {Id}\n");
+        }
+    }
+}
